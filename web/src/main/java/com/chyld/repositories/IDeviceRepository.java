a5faabd87f9ad97c327e7a4f3123287e03732e28
Bal Saini and Evan Gryska <pair+bal+evan@allstate.com>
2016-09-08 11:30:32 -0500

    split web and data

--- web/src/main/java/com/chyld/repositories/IDeviceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chyld.repositories;
-
-import com.chyld.entities.Device;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface IDeviceRepository extends PagingAndSortingRepository<Device, Integer> {
-    Device findOneBySerialNumber(String serialNumber);
-}
--- web/src/main/java/com/chyld/repositories/IPositionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chyld.repositories;
-
-
-import com.chyld.entities.Position;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface IPositionRepository extends PagingAndSortingRepository<Position, Integer>{
-}
